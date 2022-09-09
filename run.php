8f46b07c234499b93672442f763d44427d34614a
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-09-09 10:55:45 +0300

    logging

--- run.php
+++ run.php
@@ -1,1 +1,12 @@
 <?php
+
+spl_autoload_extensions('.php');
+spl_autoload_register();
+
+use App\Logger\Logger;
+
+$logDir = __DIR__ . '/' . 'Log';
+$logFileName = date('Y-m-d') . '.txt';
+
+$logger = new Logger($logDir . '/' . $logFileName);
+// echo $logDir . '/' . $logFileName;
