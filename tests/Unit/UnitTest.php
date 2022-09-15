2d165693738bc67c6de1d79c5676679ad96eb712
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-09-15 15:04:11 +0300

    test unit

--- tests/Unit/UnitTest.php
+++ tests/Unit/UnitTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Unit;
 
+use App\Interfaces\OfferCollectionInterface;
 use App\Model\Offer;
+use App\Model\OfferCollection;
+use App\Services\DataFilter;
 use PHPUnit\Framework\TestCase;
 
 class UnitTest extends TestCase {
@@ -11,9 +14,27 @@ class UnitTest extends TestCase {
 
         $data = new Offer();
 
-        $data->load(["offerId" => 123,]);
+        $data->load(["offerId" => 123]);
         $expected = 123;
  
         $this->assertEquals($expected, $data->getOfferId());
+    }
+    public function test_that_get_price() : void {
+
+        $data = new Offer();
+
+        $data->load(["price" => 390.4]);
+        $expected = 390.4;
+
+        $this->assertEquals($expected, $data->getPrice());
+    }
+    public function test_that_DataFilter_works() : void {
+        $Offer = new OfferCollectionInterface();
+        $data = new DataFilter($Offer);
+
+        $data->filterByPrice(5, 100);
+        $
+
+        
     }
 }
